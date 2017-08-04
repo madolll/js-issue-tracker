92f3668efebb4ca7bf98e92e7b7e4da97bf249bc
Madeleine Alzamora <zmadeleine12@gmail.com>
2017-08-04 19:25:25 -0400

    refactor + commented code

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
 $(document).ready(function() {
+	// Custom form behavior
+	$("form[name='issue-tracker']").off("submit");
+  $("form[name='issue-tracker']").on("submit", function(event) {
+    event.preventDefault();
 
-    $("#track-btn").on("click", function( event ) {
-      event.preventDefault();
+		// Form data
+    var description = $("[name='description']", this).val();
+    var severity = $("[name='severity']", this).val();
+    var responsible = $("[name='responsible']", this).val();
 
-      var issue = $("#issue-description").val();
-      var severity = $("#severity").val();
-      var responsible = $("#responsible").val();
-      var $template = $(".template").clone(false, false).removeClass("template hidden");
+		// Custom template
+    var $template = $(".template").clone(false, false).removeClass("template hidden");
 
-      $("[data-id='id']", $template).text(guid());
-      $("[data-id='description']", $template).text(issue);
-      $("[data-id='severity']", $template).text(severity);
-      $("[data-id='person']", $template).text(responsible);
+		// Generate a unique ID
+    $("[data-id='id']", $template).text(guid());
 
-      $("#responses").append($template);
+		// Fill the template with the form data
+		$("[data-id='description']", $template).text(description);
+    $("[data-id='severity']", $template).text(severity);
+    $("[data-id='person']", $template).text(responsible);
 
-      $("[data-id='close']", $template).on("click", function(){
-        var status = $template.attr("data-status");
-        if (status == "open") {
-          $("[data-id='status']", $template).text("closed").removeClass("btn-info").addClass("btn-success");
-          $(this).text("reopen").removeClass("btn-warning").addClass("btn-info");
-          $template.attr("data-status", "closed");
-        } else {
-          $("[data-id='status']", $template).text("open").removeClass("btn-success").addClass("btn-info");
-          $(this).text("close").removeClass("btn-info").addClass("btn-warning");
-          $template.attr("data-status", "open");
-        }
-      });
+		// Append the template to the list
+    $("#responses").append($template);
 
-      $("[data-id='delete']", $template).on("click", function(){
-        $($template).remove();
-      });
-
+		// Issue states ['open', 'closed']
+		// Default state: open
+		$("[data-id='close']", $template).off("click");
+    $("[data-id='close']", $template).on("click", function(){
+      var status = $template.attr("data-status");
+			// If the status is open trigger the closed state
+      if (status == "open") {
+        $("[data-id='status']", $template).text("closed").removeClass("btn-info").addClass("btn-success"); // Change the text and switch classes on the status
+        $(this).text("reopen").removeClass("btn-warning").addClass("btn-info"); // Change the text and switch classes on the button
+        $template.attr("data-status", "closed"); // Save the new state
+      } else {
+			// If the status is closed trigger the open state
+        $("[data-id='status']", $template).text("open").removeClass("btn-success").addClass("btn-info"); // Change the text and switch classes on the status
+        $(this).text("close").removeClass("btn-info").addClass("btn-warning"); // Change the text and switch classes on the button
+        $template.attr("data-status", "open"); // Save the new state
+      }
     });
+
+		// Remove the issue from the list
+		$("[data-id='delete']", $template).off("click");
+    $("[data-id='delete']", $template).on("click", function(){
+      $($template).remove();
+    });
+  });
 });
 
 
